6e931a23d2550aae2cf7af0ee748fb1b7ca30f56
Markku Rossi <mtr@iki.fi>
2018-11-26 08:08:51 +0200

    More events.

--- handlers/sshd.go
+++ handlers/sshd.go
@@ -35,8 +35,29 @@ var matches = []match{
 	// Accepted publickey for mtr from 10.0.2.2 port 56828 ssh2: RSA SHA256:R9D+G/DQmxLICfKYEoGTzKmgc48XLOa3iD6Fa4ecneY
 	match{
 		P: "sshd-auth-pubkey",
-		R: regexp.MustCompile(`^Accepted publickey for (\S+) from (\S+) port (\d+) ssh2: (\S+) (\S+)`),
+		R: regexp.MustCompile(`^Accepted publickey for (\S+) from (\S+) port (\d+) ssh2: (\S+) (\S+)$`),
 	},
+	// Accepted publickey for root from 10.42.0.201 port 32998 ssh2: RSA-CERT ID mtr@127.0.0.1:33872 serial 1599840225250998364 (serial 1599840225250998364) CA RSA SHA256:PADEJsxu92lFT48j4lCk1ICbaV8/hZfXQ5HAl3iTKSc
+	match{
+		P: "sshd-auth-certificate",
+		R: regexp.MustCompile(`^Accepted publickey for (\S+) from (\S+) port (\d+) ssh2: (\S+) ID (\S+) serial (\S+) \(serial (\S+\)) CA (\S+) (\S+)`),
+	},
+	// Accepted certificate ID "mtr@127.0.0.1:33338 serial 8846075489776407527" (serial 8846075489776407527) signed by RSA CA SHA256:PADEJsxu92lFT48j4lCk1ICbaV8/hZfXQ5HAl3iTKSc via /etc/ssh/privx_ca.pub
+	match{
+		P: "sshd-accepted-certificate",
+		R: regexp.MustCompile(`^Accepted certificate ID "([^"]+)" \(serial (\S+)\) signed by (\S+) CA (\S+) via (.*)`),
+	},
+	// error: key_cert_check_authority: invalid certificate
+	match{
+		P: "sshd-certificate-check-authority",
+		R: regexp.MustCompile(`^error: key_cert_check_authority: (.*)$`),
+	},
+	// error: Certificate invalid: expired
+	match{
+		P: "sshd-invalid-certificate",
+		R: regexp.MustCompile(`^error: Certificate invalid: (.*)$`),
+	},
+
 	// Failed publickey for mtr from 10.0.2.2 port 56979 ssh2: RSA SHA256:R9D+G/DQmxLICfKYEoGTzKmgc48XLOa3iD6Fa4ecneY
 	match{
 		P: "sshd-failed-pubkey",
@@ -77,6 +98,21 @@ var matches = []match{
 		P: "sshd-disconnected",
 		R: regexp.MustCompile(`^Disconnected from (\S+) port (\d+)`),
 	},
+	// Connection closed by 10.42.0.201
+	match{
+		P: "sshd-connection-closed",
+		R: regexp.MustCompile(`^Connection closed by (\S+)$`),
+	},
+	// Transferred: sent 6156, received 5544 bytes
+	match{
+		P: "sshd-transferred",
+		R: regexp.MustCompile(`^Transferred: sent (\d+), received (\d+) bytes$`),
+	},
+	// Closing connection to 10.42.0.201 port 45770
+	match{
+		P: "sshd-closing-connection",
+		R: regexp.MustCompile(`^Closing connection to (\S+) port (\d+)$`),
+	},
 
 	// pam_unix(sshd:session): session opened for user mtr by (uid=0)
 	match{
@@ -92,6 +128,11 @@ var matches = []match{
 	match{
 		P: "sshd-authentication-failure",
 		R: regexp.MustCompile(`^pam_unix\(sshd:auth\): authentication failure; logname=(\S*) uid=(\S*) euid=(\S*) tty=(\S*) ruser=(\S*) rhost=(\S*)  user=(\S*)`),
+	},
+	// pam_systemd(sshd:session): Failed to release session: Interrupted system call
+	match{
+		P: "sshd-error-session-release",
+		R: regexp.MustCompile(`^pam_systemd\(sshd:session\): Failed to release session: (.*)$`),
 	},
 }
 
